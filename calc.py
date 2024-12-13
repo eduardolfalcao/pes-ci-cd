2201158327447a15f4fdb701b871f637b590ed91
Eduardo Falcão <eduardo@dca.ufrn.br>
2024-12-13 09:49:15 -0300

    Remove bug from div op

--- calc.py
+++ calc.py
@@ -11,7 +11,7 @@ def multiply(a, b):
         return np.multiply(a, b)
 
 def divide(a, b):
-        return a/-b
+        return a/b
 
 if __name__ == "__main__":
     print(f"Addition: {add(1, 2)}")
